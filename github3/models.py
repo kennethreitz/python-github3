82c1cf1acfd86d49c025ca30c16463e19f883fbe
Kenneth Reitz <me@kennethreitz.com>
2011-04-13 19:13:19 -0400

    basic models

---
+++ github3/models.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+github3.models
+~~~~~~~~~~~~~~
+
+This module provides the GitHub3 object models.
+
+"""
+
+
+
+class Repo(object):
+    """GitHub Repository."""
+    pass
+
+
+
+class Gist(object):
+    """GitHub Gist.
+
+    gist.files['filename.py']
+    """
+
+    def __init__(self):
+        pass
+
+
+
+class GistComment(object):
+    """GitHub GistComment."""
+
+    def __init__(self):
+        pass
