3f0f06845a23a0726254fc8f0b2c6db44c38ae34
Kenneth Reitz <me@kennethreitz.com>
2011-04-13 19:41:58 -0400

    fab update

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,16 @@ import os
 from fabric.api import *
 
 
-DOCS_URL = 'https://gist.github.com/raw/71c2878f53e886dc921a/general.md'
+DOCS_URL = 'https://gist.github.com/raw/71c2878f53e886dc921a/'
 
 
 def get_docs():
     """Removed Trashcan.
     """
     os.chdir('ext')
-    os.system('curl -O {0}'.format(DOCS_URL))
+    os.system('curl -O {0}{1}'.format(DOCS_URL, 'general.md'))
+    os.system('curl -O {0}{1}'.format(DOCS_URL, 'issue_comments.md'))
+    os.system('curl -O {0}{1}'.format(DOCS_URL, 'issues.md'))
+    os.system('curl -O {0}{1}'.format(DOCS_URL, 'labels.md'))
+    os.system('curl -O {0}{1}'.format(DOCS_URL, 'milestones.md'))
 
