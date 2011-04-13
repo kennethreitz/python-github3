53c42a9fae94c26b52360398955a617976a176c3
Kenneth Reitz <me@kennethreitz.com>
2011-04-13 18:16:21 -0400

    simple core

---
+++ github3/core.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+github3.core
+~~~~~~~~~~~~
+
+This module contains the core GitHub 3 interface.
+
+"""
+
+
+class GitHub(object):
+    pass
+
+    def __init__(self):
+        pass
