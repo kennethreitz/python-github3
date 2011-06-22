be3ac031dda3812fa6d1d9d88da1a57ce8072743
Kenneth Reitz <me@kennethreitz.com>
2011-06-21 23:11:03 -0400

    bunk

--- github3/api.py
+++ github3/api.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-    github3.api
-    ~~~~~~~~~~~
+github3.api
+~~~~~~~~~~~
 
-    This module implements the GitHub3 API wrapper objects.
+This module implements the GitHub3 API wrapper objects.
 
-    :copyright: (c) 2011 by Kenneth Reitz.
-    :license: ISC, see LICENSE for more details.
+:copyright: (c) 2011 by Kenneth Reitz.
+:license: ISC, see LICENSE for more details.
+
 """
+
 
 from . import models
 from .packages.anyjson import deserialize
