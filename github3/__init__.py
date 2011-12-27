3926b07f44e091ceea4db70f27b0d47523d01a69
Kenneth Reitz <me@kennethreitz.com>
2011-12-26 22:46:32 -0500

    ASCII ART!
                                  _   
            ,      /)       /)   '  ) 
       _      _/_ (/       (/_    -(  
      (_/__(_ (__ / )_(_(_/_)  (__ )  
     .-/                              
    (_/

---
+++ github3/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+#                      I8    ,dPYb,                 ,dPYb,
+#                      I8    IP'`Yb                 IP'`Yb
+#               gg  88888888 I8  8I                 I8  8I
+#               ""     I8    I8  8'                 I8  8'
+#    ,gggg,gg   gg     I8    I8 dPgg,   gg      gg  I8 dP
+#   dP"  "Y8I   88     I8    I8dP" "8I  I8      8I  I8dP   88gg
+#  i8'    ,8I   88    ,I8,   I8P    I8  I8,    ,8I  I8P    8I
+# ,d8,   ,d8I _,88,_ ,d88b, ,d8     I8,,d8b,  ,d8b,,d8b,  ,8I
+# P"Y8888P"8888P""Y8 8P""Y8 88P     `Y88P'"Y88P"`Y88P'"Y88P"'
+#        ,d8I'
+#      ,dP'8I
+#     ,8"  8I
+#     I8   8I
+#     `8, ,8I
+#      `Y8P"
+
+
+"""
+github3
+~~~~~~~
+
+:copyright: (c) 2011 by Kenneth Reitz.
+:license: MIT, see LICENSE for more details.
+"""
+
+# Meta.
+
+__title__ = 'github3'
+__author__ = 'Kenneth Reitz'
+__copyright__ = 'Copyright 2011 Kenneth Reitz'
+__license__ = 'MIT'
+
+__version__ = '0.0.1'
+
+# Module namespace.
+
+from .core import from_pass
